724179ad94e4a034311be74fe82ffae7e3ce2707
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2024-04-23 19:01:03 +0100

    fix #31 and bump version number

--- src/main/java/games/enchanted/verticalSlabs/blockTypes/CombinableVerticalSlabBlock.java
+++ src/main/java/games/enchanted/verticalSlabs/blockTypes/CombinableVerticalSlabBlock.java
@@ -138,6 +138,14 @@ public class CombinableVerticalSlabBlock extends HorizontalFacingBlock implement
     }
     
     @Override
+    protected BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if ( state.get(Properties.WATERLOGGED).booleanValue() ) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+    
+    @Override
     protected boolean canPathfindThrough(BlockState state, NavigationType type) {
         return !state.get(SINGLE);
     }
